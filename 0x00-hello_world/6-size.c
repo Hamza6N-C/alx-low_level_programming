a574845d1bfdabc4b75d978f58c15678ebb0c892
Hamza6N-C <benhaddouhamz>
2023-02-16 14:05:28 -0800

    ahhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	int k;
-	long int l;
-	long long int m;
-	float n;
-	char p;
-printf("Size of int: %a byte(s)\n", (unsigned long)sizeof(k));
-printf("size of long int: %a byte(s)\n", (unsigned long)sizeof(l));
-printf("size of long long int: %a byte(s)\n", (unsigned long)sizeof(m));
-printf("size of float: %a byte(s)\n", (unsigned long)sizeof(n));
-printf("size of chart: %a byte(s)\n", (unsigned long)sizeof(p));
+	char k;
+	int l;
+	long int m;
+	long long int n;
+	float p;
+printf("Size of a char: %a byte(s)\n", (unsigned long)sizeof(k));
+printf("Size of a int: %a byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of long int: %a bytes(s)\n", (unsigned long)sizeof(m));
+printf("Size of a long long int: %a bytes(s)\n", (unsigned long)sizeof(n));
+printf("size of a float: %a bytes(s)\n", (unsigned long)sizeof(p));
 return (0);
 }
