ce0820b4cb4b1f196d14c515712b6503ad46d0b8
Hamza6N-C <benhaddouhamz>
2023-02-16 12:33:34 -0800

    Name Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Use the function puts and You are not allowed to use printf
+ * Return : 0 (Success)
+ */
+int main (void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
