566ae44f0578e84a8c65cdecd3af940771a99026
Hamza6N-C <benhaddouhamz>
2023-02-19 07:26:17 -0800

    unified-diff format patch

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,14 +4,16 @@
 
 /**
  * main - Entry point
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
-		putchar(a);
+	for (a = 0; a < 10; a++)
+	printf("%d", a);
+	printf("\n");
+
 	return (0);
 }
