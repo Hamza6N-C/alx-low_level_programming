e5863a7858e848ab0652f6cc755292daff8aa1b3
Hamza6N-C <benhaddouhamz>
2023-02-19 06:36:12 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of alphaBETIC
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Aa;
+
+	for (Aa = 'a'; Aa <= 'z'; Aa++)
+	putchar(Aa);
+
+	for (Aa = 'A'; Aa <= 'Z'; Aa++)
+		putchar(Aa);
+	putchar('\n');
+
+	Return(0);
+}
+
+
