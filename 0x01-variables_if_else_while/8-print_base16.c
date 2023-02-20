70f1e0f0f673a268b7801f61e299ef4090c20538
Hamza6N-C <benhaddouhamz>
2023-02-20 10:23:05 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints if the number is pos,neg or 0
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int a;
+	char l;
+
+	for (d = '0'; d <= 9; d++)
+		putchar(d);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
