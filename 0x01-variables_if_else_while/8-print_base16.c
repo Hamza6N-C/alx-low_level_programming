badb7768dc878ed9addee0e5580604c1fbea4ed3
Hamza6N-C <benhaddouhamz>
2023-02-20 12:11:21 -0800

    betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ * main - prints all the numbers of base 16 in lowercase
  * Return: ALways 0
  */
 int main(void)
