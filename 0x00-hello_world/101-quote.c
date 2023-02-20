dee10ea7078facd9fce51202aa3313ad5d96fa50
Hamza6N-C <benhaddouhamz>
2023-02-20 14:12:55 -0800

    b

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 /**
  * main - Entry
- * return 1 (Success)
+ * Return: 1 (Success)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
