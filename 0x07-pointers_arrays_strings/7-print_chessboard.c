030787c60249ca74470a5e8ee7e620bd063fd1f7
Hamza6N-C <benhaddouhamz>
2023-03-06 14:30:48 -0800

    bonus

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: array
@@ -8,6 +8,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 	for (j = 0; j < 8; j++)
