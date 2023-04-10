a162c0d346143fd3540c1d3180a8165c9d4bae2b
team project <benhaddouhamza2@gmail.com>
2023-04-10 16:07:16 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile- reads a text file and prints it to the POSIX standard output.
  * @filename: text file
  * @letters: the number of letters it should read and print
  * Return: the actual number of letters it could read and print
