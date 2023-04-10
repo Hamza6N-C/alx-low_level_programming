009b7d1f777b2ccfb83146ee459cde188d8095c2
team project <benhaddouhamza2@gmail.com>
2023-04-10 15:58:17 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * function that reads a text file and prints it to the POSIX standard output.
+ * @filename: text file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ * 0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *b;
+	ssize_t a;
+	ssize_t c;
+	ssize_t d;
+
+	a = open(filename, O_RDONLY);
+	if (a == -1)
+		return (0);
+	b = malloc(sizeof(char) * letters);
+	d = read(a, b, letters);
+	c = write(STDOUT_FILENO, b, d);
+	free(b);
+	close(a);
+	return (c);
+}
